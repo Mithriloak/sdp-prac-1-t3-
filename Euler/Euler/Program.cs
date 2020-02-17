c474f579bf5cd8275e21fe81054b13c8456d88c9
MithrilOak <caretak3r@gmail.com>
2020-02-17 14:02:01 +0200

    First Euler Problem

--- Euler/Euler/Program.cs
+++ Euler/Euler/Program.cs
@@ -10,6 +10,28 @@ namespace Euler
     {
         static void Main(string[] args)
         {
+            List<int> list = new List<int>();
+            int sum = 0;
+
+            for (int i = 0; i < 1001; i++)
+            {
+                list.Add(i);
+
+                if (i % 3 == 0)
+                {
+                    sum = sum + i;
+                }
+
+                if (i % 5 == 0)
+                {
+                    sum = sum + i;
+                }
+                
+            }
+
+            Console.WriteLine("The sum of all the multiples of 3 and 5 below 1000 is = " + sum.ToString());
+
+            Console.ReadLine();
         }
     }
 }
